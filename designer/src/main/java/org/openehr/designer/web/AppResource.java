b263ae6aaa7f41437df4b3f4d6cf42359bec4971
Marko Pipan <marko.pipan@marand.si>
2015-11-13 11:48:06 +0100

    Avoid hard-coding github api client_id in login html

---
+++ designer/src/main/java/org/openehr/designer/web/AppResource.java
@@ -0,0 +1,45 @@
+/*
+ * ADL Designer
+ * Copyright (c) 2013-2014 Marand d.o.o. (www.marand.com)
+ *
+ * This file is part of ADL2-tools.
+ *
+ * ADL2-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openehr.designer.web;
+
+import org.openehr.designer.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author markopi
+ */
+@RequestMapping("/app")
+@RestController
+public class AppResource {
+
+    @RequestMapping(value = "/configuration", method = RequestMethod.GET)
+    public Map<String, Object> getAppConfiguration() {
+        Map<String, Object> result = new LinkedHashMap<>();
+        result.put("github.api.auth.client_id", Configuration.get("github.api.auth.client_id"));
+        return result;
+    }
+}
